5b20032082379b233001bc6bbdf5905bb05be268
Francisco Serrano <ciscoserrano@icloud.com>
2024-07-26 19:45:54 -0700

    Initial Commit

---
+++ KittenTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KittenTracker
+//
+//  Created by Francisco Serrano on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KittenTracker/KittenTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittenTrackerApp.swift
+//  KittenTracker
+//
+//  Created by Francisco Serrano on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct KittenTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
